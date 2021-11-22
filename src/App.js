dcfdc743430d7a5e7ab110c8c679e25cb0e184b3
Denys Grozenok <dgrozenok@gmail.com>
2021-11-22 14:32:04 -0800

    started adding routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
+import React from 'react';
 import './App.css';
 import HomePage from "./pages/homepage/homepage.component";
+import {Routes, Route} from "react-router-dom";
+
+const HatsPage = (props) => {
+  return (
+    <div>
+      <h1>HATS PAGE</h1>
+    </div>
+  );
+};
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Routes>
+        <Route exact path="/" element={<HomePage />} />
+        <Route path="/shop/hats" element={<HatsPage />} />
+      </Routes>
     </div>
   );
 }
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,13 +1,18 @@
 import './menu-item.styles.scss';
+import { useNavigate } from "react-router-dom";
 
-export const MenuItem = ({title, imageUrl, size}) => (
-  <div className={`${size} menu-item`}>
-    <div className="background-image" style={{backgroundImage: `url(${imageUrl})`}}/>
-    <div className="content">
-      <h1 className="title">{title}</h1>
-      <span className="subtitle">SHOP NOW</span>
+export const MenuItem = ({title, imageUrl, size, linkUrl}) => {
+  const navigate = useNavigate();
+
+  return (
+    <div className={`${size} menu-item`} onClick={() => navigate(linkUrl)}>
+      <div className="background-image" style={{backgroundImage: `url(${imageUrl})`}}/>
+      <div className="content">
+        <h1 className="title">{title}</h1>
+        <span className="subtitle">SHOP NOW</span>
+      </div>
     </div>
-  </div>
-);
+  )
+};
 
 export default MenuItem;
